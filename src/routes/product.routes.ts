dd3b0f77b8335c992e8514b8efff22dcd7e5730e
Osmar Eduardo Nothaft <osmarn@gmail.com>
2023-04-10 03:32:30 -0300

    refactor - changed product controller file name
    Signed-off-by: Osmar Eduardo Nothaft <osmarn@gmail.com>

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import productController from '../controllers/productController';
+import productController from '../controllers/product.controller';
 
 
 const router = Router();
