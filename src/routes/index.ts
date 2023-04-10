58100d6b0932a0ad094af513fa98ea9c9a7876d0
Osmar Eduardo Nothaft <osmarn@gmail.com>
2023-04-10 16:06:20 -0300

    refactor - changed index routes
    Signed-off-by: Osmar Eduardo Nothaft <osmarn@gmail.com>

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,13 @@
-import express from "express"
-import { productRoutes } from "./product.routes"
+import express, { Express } from 'express'
+import { categoryRoutes } from './category.routes'
+import { productRoutes } from './product.routes'
+import { loginRoutes } from './login.routes'
+import { userRoutes } from './user.routes'
 
-export const routesApp = (app: any) => {
+export const routesApp = (app: Express) => {
     app.use(express.json());
+    userRoutes(app);
+    loginRoutes(app);
+    categoryRoutes(app);
     productRoutes(app);
 };
