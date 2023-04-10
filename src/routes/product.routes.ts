93d227195fe431d0b85eb871c1c651eca3e6e71a
Osmar Eduardo Nothaft <osmarn@gmail.com>
2023-04-10 16:04:26 -0300

    refactor - changed product routes to add auth middleware
    Signed-off-by: Osmar Eduardo Nothaft <osmarn@gmail.com>

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,15 +1,15 @@
-import { Router } from 'express';
+import { Router, Express } from 'express';
 import productController from '../controllers/product.controller';
-
+import AuthMiddleware from '../middlewares/auth.middleware'
 
 const router = Router();
 
-export const productRoutes = (app: any) => {
-    router.get('', productController.getAll);
-    router.get('/:id', productController.getOne);
-    router.post('', productController.create);
-    router.patch('/:id', productController.update);
-    router.delete('/:id', productController.delete);
+export const productRoutes = (app: Express) => {
+    router.post('', AuthMiddleware.authenticate, productController.create);
+    router.get('', AuthMiddleware.authenticate, productController.getAll);
+    router.get('/:id', AuthMiddleware.authenticate, productController.getOne);
+    router.patch('/:id', AuthMiddleware.authenticate, productController.update);
+    router.delete('/:id', AuthMiddleware.authenticate, productController.delete);
 
     app.use('/product', router);
 }
